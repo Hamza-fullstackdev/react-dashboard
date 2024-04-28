839cc4f51921ec5cb7f2d0506b53fb9b94720653
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-04-28 20:01:11 +0500

    Sidebar Design Completed

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,41 +1,73 @@
 import React from "react";
-import { Avatar, Dropdown, Sidebar } from "flowbite-react";
-import { HiChartPie } from "react-icons/hi";
+import { Avatar, Button, Dropdown, Sidebar } from "flowbite-react";
+import {
+  HiChartPie,
+  HiUser,
+  HiInbox,
+  HiArrowSmRight,
+  HiArrowCircleLeft,
+  HiViewBoards,
+  HiDownload,
+  HiShoppingBag,
+} from "react-icons/hi";
 const Sidenav = () => {
   return (
     <div className='w-[300px] min-h-screen'>
-      <Sidebar aria-label='Sidebar'>
+      <Sidebar aria-label='Sidebar' className="relative">
         <Sidebar.Items>
           <Sidebar.ItemGroup>
-            <div className='w-full flex justify-start border border-gray-200 p-2 rounded-xl gap-2'>
-              <Dropdown arrowIcon={false} inline label={<Avatar
-                  className='w-12 h-12 object-contain'
-                  img={
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-PVBbehIgurp7yUi8-YkelEqCToBGOsLKX9uOOjz5ow&s"
-                  }
-                  rounded
-                />}>
-                <Dropdown.Item>
-                <div className="flex flex-col text-start">
-                <span className='block text-sm'>Bonnie Green</span>
-                <span className='block truncate text-sm font-medium'>
-                  name@flowbite.com
-                </span>
-                </div>
-                </Dropdown.Item>
-                <Dropdown.Divider />
+            <div className='w-full flex justify-start border border-gray-200 p-2 rounded-xl gap-2 overflow-hidden mt-2'>
+              <Dropdown
+                arrowIcon={false}
+                inline
+                label={
+                  <Avatar
+                    className='w-12 h-12 object-contain'
+                    img={
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-PVBbehIgurp7yUi8-YkelEqCToBGOsLKX9uOOjz5ow&s"
+                    }
+                    rounded
+                  />
+                }
+              >
+                <Dropdown.Header>
+                  <div className='flex flex-col text-start'>
+                    <span className='block text-sm'>Hamza Ilyas</span>
+                    <span className='block truncate text-sm font-medium'>
+                      hamzailyas@gmail.com
+                    </span>
+                  </div>
+                </Dropdown.Header>
                 <Dropdown.Item>Sign out</Dropdown.Item>
               </Dropdown>
-              <div className=''>
-                <span className='block text-sm'>Bonnie Green</span>
-                <span className='block truncate text-sm font-medium'>
-                  name@flowbite.com
+              <div className='flex flex-col'>
+                <span className='text-sm'>Hamza Ilyas</span>
+                <span className='whitespace-normal text-sm font-medium'>
+                  hamzailyas@gmail.com
                 </span>
               </div>
             </div>
-            <Sidebar.Item icon={HiChartPie} label='User' labelColor='dark'>
-              Dashboard
+            <Sidebar.Item icon={HiChartPie}>Dashboard</Sidebar.Item>
+            <Sidebar.Item icon={HiUser} label='User' labelColor='dark'>
+              Profile
             </Sidebar.Item>
+            <Sidebar.Item icon={HiInbox} label='3'>
+              Inbox
+            </Sidebar.Item>
+            <Sidebar.Collapse icon={HiShoppingBag} label='E-commerce'>
+              <Sidebar.Item href='#'>Products</Sidebar.Item>
+              <Sidebar.Item href='#'>Sales</Sidebar.Item>
+              <Sidebar.Item href='#'>Refunds</Sidebar.Item>
+              <Sidebar.Item href='#'>Shipping</Sidebar.Item>
+            </Sidebar.Collapse>
+            <Sidebar.Item icon={HiViewBoards}>Report</Sidebar.Item>
+            <Sidebar.Item icon={HiArrowSmRight}>Sign In</Sidebar.Item>
+            <Sidebar.Item icon={HiArrowCircleLeft}>Sign out</Sidebar.Item>
+            <div className="w-[230px]" style={{position:"absolute",bottom:'2%'}}>
+              <Button className='w-full' color={"dark"}>
+                <HiDownload className='mr-1 h-4 w-4' /> Download
+              </Button>
+            </div>
           </Sidebar.ItemGroup>
         </Sidebar.Items>
       </Sidebar>
