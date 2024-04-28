8e8053e92a211ddd225b439486636b095ac76b80
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-04-28 18:12:25 +0500

    Create Sidebar And Main Content Components

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Sidebar from "../components/Sidebar";
+import Main from "../components/Main";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Sidebar />
+      <Main />
+    </div>
+  );
 };
 
 export default Dashboard;
